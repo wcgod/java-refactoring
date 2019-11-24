e935abf3e4eaf83e7d9ef9b20103b85b926bf813
wcgod <wcgod90@gmail.com>
2019-11-25 01:28:59 +0900

    refactoring example1 (StringCalculator 작성)

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int result = 0;
+
+        //문자열 계산 클래스 호출
+        result = StringCalculator.splitAndSum("1,2,3");
+
+        //결과값 출력
+        System.out.println(result);
+    }
+}
