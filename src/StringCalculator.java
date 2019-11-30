14fbaced6b5f17a9f26501cc9353a19ef071bbb1
wcgod <wcgod90@gmail.com>
2019-11-30 22:36:57 +0900

    refactoring example1-3 (메소드는 한가지 역할만 수행)

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -7,7 +7,8 @@ public class StringCalculator {
             return 0;
         }
         String[] values = text.split(",|:");
-        return sum(values);
+        int[] numbers = toInts(values);
+        return sum(numbers);
         /*
         } else {
             String[] values = text.split(",|:");
@@ -24,10 +25,28 @@ public class StringCalculator {
         */
     }
 
+    /* 메소드는 한가지 일만 하도록 수정
     private static int sum(String[] values) {
         int result = 0;
         for (String value : values) {
             result += Integer.parseInt(value);
+        }
+        return result;
+    }
+    */
+
+    private static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i=0;i < values.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
+        }
+        return numbers;
+    }
+
+    private static int sum(int[] numbers) {
+        int result = 0;
+        for (int number : numbers) {
+            result += number;
         }
         return result;
     }
